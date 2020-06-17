159e2c67898a5d1bfbd173437d990ee060dc3567
acvp3lla <lucinaogordon@hotmail.com>
2020-06-17 12:28:57 -0500

    added conditional rendering

--- src/Components/HOCs/withCount.js
+++ src/Components/HOCs/withCount.js
@@ -1,14 +1,24 @@
-import React, {useState, Component} from 'react'
+import React, {Component} from 'react'
 
 class Count extends Component{
     constructor(){
         super()
         this.state = {
-            count: 0
+            count: 0,
+            on: true
         }
 
         this.increment = this.increment.bind(this)
         this.decrement = this.decrement.bind(this)
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle() {
+        this.setState(prevState => {
+            return {
+                on: !prevState.on
+            }
+        })
     }
     
     increment() {
@@ -31,7 +41,7 @@ class Count extends Component{
         const Component = this.props.component
         return(
             <div>
-                <Component increment={this.increment} decrement={this.decrement} count={this.state.count} {...this.props}/>
+                <Component increment={this.increment} decrement={this.decrement} count={this.state.count} show={this.state.on} toggle={this.toggle} {...this.props}/>
             </div>
         )
     }
--- src/Components/Number.js
+++ src/Components/Number.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import {withCount} from './HOCs/withCount'
+import './Number.css'
 
 function Number (props){
     return(
         <div>
-            <h1>{props.count}</h1>
-            <button onClick={props.increment}>Increment</button>
+            <button className="myButton" onClick={props.toggle}>{props.show? 'Hide' : 'Show'}</button>
             <br/>
-            <button onClick={props.decrement}>Decrement</button>
+            <div style={{display: props.show? 'block' : 'none'}}>
+                <h1>{props.count}</h1>
+            </div>
+            <button className="myButton" onClick={props.increment}>Increment</button>
+            <br/>
+            <button className="myButton" onClick={props.decrement}>Decrement</button>
         </div>
     )
 }
